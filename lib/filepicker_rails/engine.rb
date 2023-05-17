3a2cd6dc6fa621b7f57bbc8703003f65965eb7f7
Jan Kaczorowski <jan.kaczorowski@gmail.com>
2023-05-17 07:14:58 +0200

    commenting out part problematic for Rails 7

--- lib/filepicker_rails/engine.rb
+++ lib/filepicker_rails/engine.rb
@@ -4,14 +4,14 @@ module FilepickerRails
     config.filepicker_rails = FilepickerRails::Configuration.new
     isolate_namespace FilepickerRails
 
-    initializer "filepicker_rails.form_builder" do
-      ActionView::Helpers::FormBuilder.send(:include, FilepickerRails::FormHelper)
-    end
+    # initializer "filepicker_rails.form_builder" do
+    #   ActionView::Helpers::FormBuilder.send(:include, FilepickerRails::FormHelper)
+    # end
 
-    initializer 'filepicker_rails.action_controller' do |app|
-      ActiveSupport.on_load(:action_controller) do
-        helper FilepickerRails::ApplicationHelper
-      end
-    end
+    # initializer 'filepicker_rails.action_controller' do |app|
+    #   ActiveSupport.on_load(:action_controller) do
+    #     helper FilepickerRails::ApplicationHelper
+    #   end
+    # end
   end
 end
